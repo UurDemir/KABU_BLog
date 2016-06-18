eb004da5fabc0b3d9d0861b00d427f7250c521e6
Aybars Arslan <16532094:S>
2016-06-18 19:12:50 +0300

    Tag repository added

---
+++ KABU_Blog/Blog.Repositories/ITagRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Repositories.Commons;
+
+namespace Blog.Repositories
+{
+    public interface ITagRepository : IGenericRepository<Tag>
+    {
+    }
+}
