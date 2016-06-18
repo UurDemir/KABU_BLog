aa0dfd7fed3c3388ecc4fb5af00085b6e6728537
Aybars Arslan <16532094:S>
2016-06-18 20:06:52 +0300

    Setting service added

---
+++ KABU_Blog/Blog.Services/ISettingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Services.Commons;
+
+namespace Blog.Services
+{
+    public interface ISettingService : IEntityService<Setting>
+    {
+        Task<Setting> FindById<TProperty>(string id, params Expression<Func<Setting, TProperty>>[] includes);
+    }
+}
---
+++ KABU_Blog/Blog.Services/SettingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Repositories.Commons;
+using Blog.Services.Commons;
+using Blog.Services.UnitOfWork;
+
+namespace Blog.Services
+{
+    public class SettingService : EntityService<Setting>, ISettingService
+    {
+        public SettingService(IUnitOfWork unitOfWork, IGenericRepository<Setting> repository ) : base(unitOfWork, repository)
+        {
+            
+        }
+
+
+        public Task<Setting> FindById<TProperty>(string id, params Expression<Func<Setting, TProperty>>[] includes)
+        {
+            return FindBy(x => x.Id == id, includes);
+        }
+    }
+}
