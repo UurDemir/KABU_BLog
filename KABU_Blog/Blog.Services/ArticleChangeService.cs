73c9e4acda5f3b1c21c3bdc4423bbd6f2d96bb39
Kagan Kaptan <kagan.kaptan@tgworkshop.com>
2016-06-18 20:04:26 +0300

    service interface added

---
+++ KABU_Blog/Blog.Services/ArticleChangeService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Repositories.Commons;
+using Blog.Services.Commons;
+using Blog.Services.UnitOfWork;
+
+namespace Blog.Services
+{
+    public class ArticleChangeService : EntityService<ArticleChange>, IArticleChangeService
+    {
+        public ArticleChangeService(IUnitOfWork unitOfWork, IGenericRepository<ArticleChange> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public Task<ArticleChange> FindById<TProperty>(int id, params Expression<Func<ArticleChange, TProperty>>[] includes)
+        {
+            return FindBy(x => x.Id == id, includes);
+        }
+    }
+}
---
+++ KABU_Blog/Blog.Services/IArticleChangeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Services.Commons;
+
+namespace Blog.Services
+{
+    public interface IArticleChangeService : IEntityService<ArticleChange>
+    {
+        Task<ArticleChange> FindById<TProperty>(int id, params Expression<Func<ArticleChange, TProperty>>[] includes);
+    }
+}
