d86484fffe4f4979be1a38b696cc3678d2bf66d1
burak.koyun <burak.koyun@tgworkshop.com>
2016-06-18 16:06:11 +0300

    Rating models added

---
+++ KABU_Blog/Blog.Models/Rating.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Models.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    class Rating : Entity<string>
+    {
+        public string UserIP { get; set; }
+
+        public string Rate { get; set; }
+
+        #region Foreign Key(s)
+
+
+        #endregion
+        public string ArticleId { get; set; }
+    }
+}
