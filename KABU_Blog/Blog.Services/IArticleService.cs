00e163e457a7838fed35a51abf8eb525bf4c77d2
Aybars Arslan <16532094:S>
2016-06-18 20:01:52 +0300

    Page and article service added

---
+++ KABU_Blog/Blog.Services/IArticleService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Services.Commons;
+
+namespace Blog.Services
+{
+    public interface IArticleService : IEntityService<Article>
+    {
+        Task<Article> FindById<TProperty>(int id, params Expression<Func<Article, TProperty>>[] includes);
+    }
+}
---
+++ KABU_Blog/Blog.Services/PageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Repositories.Commons;
+using Blog.Services.Commons;
+using Blog.Services.UnitOfWork;
+
+namespace Blog.Services
+{
+    public class PageService : EntityService<Page>, IPageService
+    {
+        public PageService(IUnitOfWork unitOfWork, IGenericRepository<Page> repository) : base(unitOfWork, repository)
+        {
+
+        }
+
+        public Task<Page> FindById<TProperty>(int id, params Expression<Func<Page, TProperty>>[] includes)
+        {
+            return FindBy(x => x.Id = id, includes);
+        }
+    }
+}
