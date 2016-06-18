ac24cc6b0bf4cddf7e6586b7d39134e4ec3cf332
Aybars Arslan <16532094:S>
2016-06-18 16:29:45 +0300

    Code clean-up

--- KABU_Blog/Blog.Models/ArticleInfo.cs
+++ KABU_Blog/Blog.Models/ArticleInfo.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Blog.Models.Commons;
 using Blog.Resources;
 
@@ -12,16 +7,16 @@ namespace Blog.Models
 {
     public class ArticleInfo : Entity<int>
     {
-        [Display(ResourceType = typeof(Displays), Name = "Key")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Key")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Key { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "Value")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Value")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Value { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "Extra")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Extra")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Extra { get; set; }
 
         #region Foreign Key(s)
--- KABU_Blog/Blog.Models/Contact.cs
+++ KABU_Blog/Blog.Models/Contact.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Blog.Models.Commons;
 using Blog.Models.Types;
 using Blog.Resources;
@@ -14,35 +7,35 @@ namespace Blog.Models
 {
     public class Contact : Entity<int>
     {
-        [Display(ResourceType = typeof(Displays), Name = "FullName")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "FullName")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Fullname { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "Email")]
-        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",ErrorMessageResourceType = typeof(Messages),ErrorMessageResourceName = "Regex")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Email")]
+        [RegularExpression(
+            @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Regex")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Email { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "PhoneNumber")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "PhoneNumber")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string PhoneNumber { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "Title")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Title")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Title { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "Message")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Message")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string Message { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "UserIp")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "UserIp")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public string UserIp { get; set; }
 
-        [Display(ResourceType = typeof(Displays), Name = "Status")]
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
+        [Display(ResourceType = typeof (Displays), Name = "Status")]
+        [Required(ErrorMessageResourceType = typeof (Messages), ErrorMessageResourceName = "Required")]
         public ContactStatus status { get; set; }
-
-
     }
 }
