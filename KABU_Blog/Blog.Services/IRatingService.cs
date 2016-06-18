926c5dc55f640ebf3eabeadf69bc04e202e0dde4
Kagan Kaptan <kagan.kaptan@tgworkshop.com>
2016-06-18 20:08:15 +0300

    rating service added

---
+++ KABU_Blog/Blog.Services/IRatingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Services.Commons;
+
+namespace Blog.Services
+{
+    public interface IRatingService : IEntityService<Rating>
+    {
+        Task<Rating> FindById<TProperty>(string id, params Expression<Func<Rating, TProperty>>[] includes);
+    }
+}
---
+++ KABU_Blog/Blog.Services/RatingService.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Blog.Models;
+using Blog.Repositories.Commons;
+using Blog.Services.Commons;
+using Blog.Services.UnitOfWork;
+
+namespace Blog.Services
+{
+    class RatingService : EntityService<Rating>, IRatingService
+    {
+        public RatingService(IUnitOfWork unitOfWork, IGenericRepository<Rating> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public Task<Rating> FindById<TProperty>(string id, params Expression<Func<Rating, TProperty>>[] includes)
+        {
+            return FindBy(x => x.Id == id, includes);
+        }
+    }
+}
