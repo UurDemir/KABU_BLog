7a2d1dd135ab34b809a4ef192cbd3c80c8a89934
burak.koyun <burak.koyun@tgworkshop.com>
2016-06-18 16:38:22 +0300

    Bug fix

--- KABU_Blog/Blog.Models/Settings.cs
+++ KABU_Blog/Blog.Models/Settings.cs
@@ -1,6 +1,8 @@
 ﻿using Blog.Models.Commons;
+using Blog.Resources;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -8,13 +10,17 @@ using System.Threading.Tasks;
 
 namespace Blog.Models
 {
-    public class Settings: Entity<string>
+    public class Setting: Entity<string>
     {
         [Column("Key")]
         public override string Id { get; set; }
 
+        [Display(ResourceType = typeof(Displays), Name = "Value")]
+        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
         public string Value { get; set; }
 
+        [Display(ResourceType = typeof(Displays), Name = "Extra")]
+        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
         public string Extra { get; set; }
 
 
--- KABU_Blog/Blog.Models/SocialMedias.cs
+++ KABU_Blog/Blog.Models/SocialMedias.cs
@@ -10,15 +10,16 @@ using System.Threading.Tasks;
 
 namespace Blog.Models
 {
-    class SocialMedias : Entity<string>
+    public class SocialMedia : Entity<string>
     {
         [Display(ResourceType = typeof(Displays), Name = "Name")]
         [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
         public string Name { get; set; }
 
-
+        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
         public string Icon { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = "Required")]
         public string Link { get; set; }
 
         [Display(ResourceType = typeof(Displays), Name = "Status")]
