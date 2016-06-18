520ebdf09901b5c2ad23d0fffa3823892e5a2e7e
ugur.demir <34ugur34>
2016-06-18 14:50:21 +0300

    Projects added

---
+++ KABU_Blog/Blog.AI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog.AI.Startup))]
+namespace Blog.AI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
