a58bc5fde8ce35d03fc193d3c7250f5abace83dd
burak.koyun <burak.koyun@tgworkshop.com>
2016-06-18 15:57:43 +0300

    Settings and socialMedias models added

---
+++ KABU_Blog/Blog.Models/Settings.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Models.Commons;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    class Settings: Entity<string>
+    {
+        [Column("Key")]
+        public override string Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string Extra { get; set; }
+
+
+    }
+}
