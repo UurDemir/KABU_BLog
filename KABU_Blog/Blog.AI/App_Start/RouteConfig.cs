514f655481369f318cbb24b88471a1b6d83df2a1
ugur.demir <34ugur34>
2016-06-19 17:14:08 +0300

    Update

--- KABU_Blog/Blog.AI/App_Start/RouteConfig.cs
+++ KABU_Blog/Blog.AI/App_Start/RouteConfig.cs
@@ -9,6 +9,8 @@ namespace Blog.AI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
