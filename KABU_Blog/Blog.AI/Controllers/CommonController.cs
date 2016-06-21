ce665b4cb4472b9c5d04ab7acf46415ee140d75c
UurDemir <uurdemir@outlook.com>
2016-06-22 00:11:52 +0300

    Navigation and Correct field makes green

--- KABU_Blog/Blog.AI/Controllers/CommonController.cs
+++ KABU_Blog/Blog.AI/Controllers/CommonController.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Blog.Mvc;
 using Microsoft.AspNet.Identity;
 
 namespace Blog.AI.Controllers
 {
-    public class CommonController : Controller
+    public class CommonController : KabuController
     {
         public ActionResult _LoginPartial()
         {
@@ -19,7 +21,16 @@ namespace Blog.AI.Controllers
             return PartialView("_Header");
         }
 
+        public ActionResult _Navigation()
+        {
+            dynamic navigation = new ExpandoObject();
+            navigation.controller = GetRoute("controller",true);
+            navigation.action = GetRoute("action",true);
 
+
+            return PartialView("_Navigation", navigation);
+        }
+        
         public ActionResult _Notifications()
         {
             return PartialView("_Notifications");
--- KABU_Blog/Blog.Mvc/KABUController.cs
+++ KABU_Blog/Blog.Mvc/KABUController.cs
@@ -1,9 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Web.Mvc;
 
 namespace Blog.Mvc
 {
-    public class KabuController:Controller
+    public class KabuController : Controller
     {
         public string RenderRazorViewToString(string viewName, object model)
         {
@@ -18,6 +19,17 @@ namespace Blog.Mvc
                 viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
                 return sw.GetStringBuilder().ToString();
             }
+        }
+
+        public string GetRoute(string routeName, bool fromParent = false)
+        {
+            object routeValue;
+            var routeDatas = fromParent ? ControllerContext.ParentActionViewContext.RouteData.Values : RouteData.Values;
+
+            var isExists = routeDatas.TryGetValue(routeName, out routeValue);
+            if (isExists)
+                return routeValue.ToString();
+            throw new KeyNotFoundException();
         }
     }
 }
