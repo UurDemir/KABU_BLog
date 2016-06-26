03f56c4bdb8b1fd998b8091931d2ec75db4f0d10
Aybars <aybars.arslan@tgworkshop.com>
2016-06-26 19:57:48 +0300

    Settings

---
+++ KABU_Blog/Blog.AI/Controllers/BlockerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.AI.Controllers
+{
+    public class BlockerController : Controller
+    {
+        // GET: Blocker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
