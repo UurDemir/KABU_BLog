3e14f6a983a35619a709da08fe705ee72cfac724
Aybars <aybars.arslan@tgworkshop.com>
2016-06-26 16:24:34 +0300

    Setting is ok

--- KABU_Blog/Blog.AI/Controllers/SettingController.cs
+++ KABU_Blog/Blog.AI/Controllers/SettingController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Blog.AI.Models;
+using Blog.Models;
+using Blog.Models.Types;
 using Blog.Mvc;
 using Blog.Services;
 
@@ -18,9 +21,37 @@ namespace Blog.AI.Controllers
         }
 
         // GET: Setting
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(Setting model)
+        {
+            if (!ModelState.IsValid)
+                return Json(new { IsCompleted = false }, JsonRequestBehavior.AllowGet);
+
+           model.Status = Status.Active;
+
+            _settingService.Create(model);
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Settings()
+        {
+            var data = _settingService.Get().Result;
+
+            var model = new TableViewModel<Setting>();
+
+            model.Hits = data.OrderBy(x => x.Id).Skip((model.CurrentPage - 1)*model.Perpage).Take(model.Perpage).ToList();
+            model.TotalCount = data.ToList().Count;
+            
+            return PartialView(model);
+            
         }
     }
 }
--- KABU_Blog/Blog.Models/Commons/Entity.cs
+++ KABU_Blog/Blog.Models/Commons/Entity.cs
@@ -3,13 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models.Commons
 {
-    public class Entity<TId>:BaseEntity,IEntity<TId>
+    public class Entity<TId> : BaseEntity, IEntity<TId>
     {
         [Key]
         [Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual TId Id { get; set; }
 
+        [NotMapped]
         public virtual string SlugId => $"{Id}";
     }
 }
